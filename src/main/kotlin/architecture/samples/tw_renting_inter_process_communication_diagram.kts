5a2e281cc1ec843a0b1ba26aeffdd601e1359afe
izhangtao <tao.zhang1@thoughtworks.com>
2022-01-23 12:01:58 +0800

    修改颜色值

--- src/main/kotlin/architecture/samples/tw_renting_inter_process_communication_diagram.kts
+++ src/main/kotlin/architecture/samples/tw_renting_inter_process_communication_diagram.kts
@@ -2,29 +2,29 @@ import architecture.diagram_inter_process
 
 diagram_inter_process {
 
-    service("前端", "#Cyan") {
+    service("前端", "#819B69") {
         process("链家租房通用版Web端").call("链家租房WebBFF","1. GET /web-bff/ads")
         process("链家租房通用版Web端").call("ADX数据监测系统","5. GET /adx/xxx")
     }
 
-    service("BFF", "#RoyalBlue") {
+    service("BFF", "#5F6BCC") {
         process("链家租房WebBFF").call("租赁信息应用服务","2. GET /rental/ads")
     }
 
-    service("应用服务", "#LightSeaGreen") {
+    service("应用服务", "#69A5D5") {
         process("租赁信息应用服务").call("鉴权认证服务","3. GET /auth/check")
         process("租赁信息应用服务").call("房屋信息管理系统","4. GET /listings/ads")
     }
 
-    service("技术组件", "#RoyalBlue") {
+    service("技术组件", "#5898A4") {
         process("三方服务网关").call("信息推广服务","7. POST /promotion-contracts/{id}/promotion/confirmation")
     }
 
-    service("核心业务能力", "#HotPink") {
+    service("核心业务能力", "#DD6779") {
         process("信息推广服务")
     }
 
-    service("领域服务", "#orange") {
+    service("领域服务", "#D57F3F") {
         process("房屋信息管理系统")
         process("鉴权认证服务")
     }
